f5042fbb61a976227c975173278a428506fc6bc3
Liane Chan <liane.chan@gmail.com>
2018-05-27 20:46:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,7 @@
 
 puts "Welcome to Tic Tac Toe"
 
-row = [" ", " ", " "]
-row1 = [ " ", " ", " "]
-row2 = [ " "," ", " "]
+cell_1
 
 board = [row[0], row[1], row[2], row1[0],  row1[1], row1[2] row2[0], row2[1], row2[2]]
 def display_board =  "|"  puts "------------"
